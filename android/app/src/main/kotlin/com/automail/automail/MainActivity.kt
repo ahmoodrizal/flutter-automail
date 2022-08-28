91a6d252decc592ac353b46d36af0a16015f76fc
AhmadRizal <amdrizalc@gmail.com>
2022-08-28 18:16:34 +0700

    Init Project with GetX CLI

---
+++ android/app/src/main/kotlin/com/automail/automail/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.automail.automail
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultmargin = 30.0;
+
+Color whiteColor = Color(0xffF9F7F7);
+Color primaryColor = Color(0xff112D4E);
+Color alertBackground = Color(0xffFFF3CD);
+Color yellowColor = Color(0xff856404);
+
+TextStyle darkTextStyle = GoogleFonts.poppins(color: primaryColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle yellowTextStyle = GoogleFonts.poppins(color: yellowColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
